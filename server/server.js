8cc376a2aeea8937c63bdbba56ddb90b20aa64b7
mbungeroth <mbungeroth@gmail.com>
2018-09-20 23:10:22 -0400

    finish status route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MTA_API_KEY = require('../secrets');
 const utils = require('./utils');
+const striptags = require('striptags');
 const app = express();
 const Mta = require('mta-gtfs');
 const mta = new Mta({
@@ -8,10 +9,39 @@ const mta = new Mta({
   feed_id: 1                  // optional, default = 1
 });
 
-app.get('/status', async (req, res) => {
+//gets all status updates on all lines (only includes problems)
+app.get('/status/', async (req, res) => {
   try {
-    const status = await mta.status('subway');
-    res.json(status)
+    const statusResults = await mta.status('subway');
+    const status = statusResults.filter(statement => statement["status"] !== "GOOD SERVICE").map(notice => {
+      return ({
+        lines: notice["name"],
+        type: notice["status"],
+        info: utils.cleanText(striptags(notice["text"]))
+      })
+    });
+    res.send(status)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+//gets trains and arrival times between 0-30 min for a specific station and direction
+app.get('/station/:stationId/:direction', async (req, res) => {
+  try {
+    const station = req.params.stationId;
+    const direction = req.params.direction;
+    const stationResults = await mta.schedule(station, 1);
+    const lastUpdated = stationResults["updatedOn"];
+    const incomingTrainData = stationResults["schedule"][station][direction].filter(train => train["arrivalTime"] >= lastUpdated && (train["arrivalTime"] - utils.currentEpochTime()) <= 1800);
+    const incomingTrains = incomingTrainData.map(incomingTrain => {
+      return ({
+        train: incomingTrain["routeId"],
+        delay: incomingTrain["delay"],
+        time: utils.untilArrival(incomingTrain["arrivalTime"]),
+      })
+    })
+    res.send(incomingTrains)
   } catch (error) {
     console.log(error)
   }
@@ -31,26 +61,6 @@ app.get('/', async (req, res) => {
     })
     }).filter(station => station["stationId"].length === 3);
     res.json(results)
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-app.get('/station/:stationId/:direction', async (req, res) => {
-  try {
-    const station = req.params.stationId;
-    const direction = req.params.direction;
-    const stationResults = await mta.schedule(station, 1);
-    const lastUpdated = stationResults["updatedOn"];
-    const incomingTrainData = stationResults["schedule"][station][direction].filter(train => train["arrivalTime"] >= lastUpdated && (train["arrivalTime"] - utils.currentEpochTime()) <= 1800);
-    const incomingTrains = incomingTrainData.map(incomingTrain => {
-      return ({
-        train: incomingTrain["routeId"],
-        delay: incomingTrain["delay"],
-        time: utils.untilArrival(incomingTrain["arrivalTime"]),
-      })
-    })
-    res.send(incomingTrains)
   } catch (error) {
     console.log(error)
   }
--- server/utils.js
+++ server/utils.js
@@ -2,15 +2,35 @@ const convertTime = epoch => new Date(epoch * 1000).toLocaleTimeString();
 const currentEpochTime = () => Math.floor(Date.now() / 1000);
 const untilArrival = arrivalTime => {
   const minutesUntil = Math.floor((arrivalTime - currentEpochTime()) / 60);
-  if (minutesUntil === 1) {
+  if (minutesUntil === 0) {
+    return "Arriving Now"
+  } else if (minutesUntil === 1) {
     return "1 Minute";
   } else {
-    return `${minutesUntil} Minutes.`
+    return `${minutesUntil} Minutes`
   }
+};
+const cleanText = text => {
+  return (
+    text
+      .replace(/  +/g, ' ')
+      .replace(/(&nbsp;)/g, '')
+      .replace(/\r/g,"")
+      .split(/\n/)
+      .reduce((arr, current) => {
+        if (current.match(/[a-z]/i)) {
+          arr.push(current.trim());
+          return arr;
+        } else {
+          return arr;
+        }
+      }, [])
+  )
 }
 
 module.exports = {
   convertTime,
   currentEpochTime,
   untilArrival,
+  cleanText,
 }
